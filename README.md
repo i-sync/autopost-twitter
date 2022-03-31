6e0a82fa338d76839509a602a6d1529d411bdd45
code-server <code-server@mail.com>
2022-03-31 11:53:38 +0800

    finish auto post twitter.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# 每天特定时间执行该脚本，自动发布天气，历史上的今天等信息到TWITTER
-## 天气
-页面URL: [http://m.weather.com.cn/mweather/101010300.shtml](http://m.weather.com.cn/mweather/101010300.shtml)
+## 每天特定时间执行该脚本，自动发布天气，历史上的今天等信息到TWITTER
+### 天气
+天气URL: [http://m.weather.com.cn/mweather/101010300.shtml](http://m.weather.com.cn/mweather/101010300.shtml)
 
 请求接口，需要指定header, Referer:http://m.weather.com.cn/ , 否则返回403
 [https://d1.weather.com.cn/sk_2d/101010100.html?_=1648633609931](https://d1.weather.com.cn/sk_2d/101010100.html?_=1648633609931)
@@ -10,6 +10,31 @@
 var dataSK={"nameen":"beijing","cityname":"北京","city":"101010100","temp":"9","tempf":"48","WD":"东北风","wde":"NE","WS":"2级","wse":"7km\/h","SD":"65%","sd":"65%","qy":"1022","njd":"17km","time":"18:10","rain":"0","rain24h":"0","aqi":"25","aqi_pm25":"25","weather":"多云","weathere":"Cloudy","weathercode":"d01","limitnumber":"1和6","date":"03月30日(星期三)"}
 ```
 
-## 历史上的今天
-前面已经从中文维基百科上的历史上的今天抓取了所有的数据并生成了JSON文件，现在拿过来直接使用，
+### 历史上的今天
+前面已经从中文维基百科上的历史上的今天抓取了所有的数据并生成了JSON文件，现在拿过来直接使用，  
 详细获取数据，请参考：[https://github.com/i-sync/history_of_today](https://github.com/i-sync/history_of_today)
+
+### Twitter 开发者账号
+首先要申请Twitter开发账号 [https://developer.twitter.com/](https://developer.twitter.com/)  
+创建Project,APP,并把APP的环境设置为PRODUCTION，  
+使用比较流行的第三方python SDK tweepy,  
+Github : [https://github.com/tweepy/tweepy](https://github.com/tweepy/tweepy)  
+文档：[https://docs.tweepy.org/en/latest/](https://docs.tweepy.org/en/latest/)  
+Twitter认证方式：使用oauth1.0a [https://docs.tweepy.org/en/latest/authentication.html#oauth-1-0a-user-context](https://docs.tweepy.org/en/latest/authentication.html#oauth-1-0a-user-context)  
+```python
+import tweepy
+
+client = tweepy.Client(
+    consumer_key="API / Consumer Key here",
+    consumer_secret="API / Consumer Secret here",
+    access_token="Access Token here",
+    access_token_secret="Access Token Secret here"
+)
+```
+
+### 发布Twitter示例
+```
+[天气自动播报] 北京, 今天是03月31日(星期四) 晴 北风4级, 当前温度: 13°C, 空气质量: 19 (优)
+
+[历史上的今天] 1964年3月31日 巴西军事政变，军人统治时期开始。
+```
--- main.py
+++ main.py
@@ -1,1 +1,100 @@
 # coding=utf-8
+
+"""
+Auto Post Twitter
+"""
+import re
+import os
+import time
+import json
+import tweepy
+import random
+import requests
+from config import configs
+
+def get_aqi_txt(num):
+    if num < 50:
+        return "优"
+    elif num < 100:
+        return "良"
+    elif num < 150:
+        return "轻度污染"
+    elif num < 200:
+        return "中度污染"
+    elif num < 300:
+        return "重度污染"
+    else:
+        return "严重污染"
+
+
+def weather():
+    url = f"https://d1.weather.com.cn/sk_2d/101010100.html?_={time.time()}"
+    headers = {
+        "Referer": "http://m.weather.com.cn/"
+    }
+    res = requests.get(url, headers=headers)
+    if res.status_code != 200:
+        return "获取天气数据失败！"
+    try:
+        res = res.content.decode('utf-8').replace("var dataSK=", "")
+        res = json.loads(res)
+        aqi = int(res["aqi_pm25"])
+        aqi_txt = get_aqi_txt(aqi)
+
+        return f'{res["cityname"]}, 今天是{time.localtime().tm_year}年{res["date"]} {res["weather"]} {res["WD"]}{res["WS"]}, 当前温度: {res["temp"]}°C, 空气质量: {aqi} ({aqi_txt})'
+    except Exception as e:
+        return "获取天气数据失败！" + str(e)
+
+def history():
+    year = time.localtime().tm_year
+    month = time.localtime().tm_mon
+    day = time.localtime().tm_mday
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    json_file = f"{dir_path}/history-of-today/{month}-{day}.json"
+    if not os.path.exists(json_file):
+        return "获取历史上的今天失败！文件不存在"
+    #index = random.randint(0, 2)
+    with open(json_file, 'r', encoding='utf-8') as f:
+        json_data = json.load(f)
+        json_data = json_data["res"][0]
+        name = json_data["name"]
+        while True:
+            i = random.randint(0, len(json_data["lists"]) -1)
+            title = json_data["lists"][i]["title"].replace("\n", "")
+            year = json_data["lists"][i]["year"]
+            if title:
+                break
+        if year == "None" and re.search("(\d{2,4})年", title):
+            year = re.search("(\d{2,4})年", title).group(1)
+        title = re.sub(f"{year}年[:， ]?", f"{year}年{month}月{day}日 ", title)
+        return title
+'''
+def test():
+    for month in range(1, 13):
+        for day in range(1, 32):
+            if month == 2 and day > 29 or month in [4,6,9,11] and day > 30:
+                continue
+            dir_path = os.path.dirname(os.path.realpath(__file__))
+            json_file = f"{dir_path}/history-of-today/{month}-{day}.json"
+            with open(json_file, 'r', encoding='utf-8') as f:
+                json_data = json.load(f)
+                json_data = json_data["res"][0]
+                for item in json_data["lists"]:
+                    title = item["title"].replace("\n", "")
+                    if title:
+                        year = item["year"]
+                        if year == "None" and re.search("(\d{2,4})年", title):
+                            year = re.search("(\d{2,4})年", title).group(1)
+                        title = re.sub(f"{year}年[:， ]?", f"{year}年{month}月{day}日 ", title)
+                        if title.find(f"{year}年{month}月{day}日 ") == -1:
+                            print(month,day , "----", title)
+'''
+if __name__ == "__main__":
+    w = weather()
+    h = history()
+    text = f"[天气自动播报] {w}\n\n[历史上的今天] {h}"
+    client=tweepy.Client(consumer_key=configs.twitter.apikey,
+                     consumer_secret=configs.twitter.apikey_secret,
+                     access_token=configs.twitter.access_token,
+                     access_token_secret=configs.twitter.access_token_secret)
+    client.create_tweet(text=text)
